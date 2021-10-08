baa641087be2edd88c84ff6d127bad2dd5418d9f
Zach Quinn <zacharytq@gmail.com>
2021-10-07 22:05:50 -0400

    Saved string to variable

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,11 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = 
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line")
+    
+        println!("You guessed: {}", guess);
 }
