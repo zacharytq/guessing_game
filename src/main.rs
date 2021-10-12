1537f8adfe9dcf5fcd1f6fb87d8e72358803c226
Zachary Quinn <zacharytq@gmail.com>
2021-10-12 13:28:53 -0400

    shadowed guess variable to make it comparable

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,11 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
         println!("You guessed: {}", guess);
     
     match guess.cmp(&secret_number) {
